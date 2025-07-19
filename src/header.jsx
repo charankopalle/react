9340f841a62c15aecb76880965476001047c464b
charankopalle <charankopalle1000@gmail.com>
2025-07-19 11:28:31 +0530

    first commit

---
+++ src/header.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+function Header(props) {
+    // var head = props.a
+    return(
+        <div>
+<h1>{props.a}</h1>
+
+        </div>
+    )
+}
+export default Header
---
+++ src/nav.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Nav(){
+  return(
+    <div id="Nav">
+      <h2>Home</h2>
+      <h2>Login</h2>
+      <h2>about</h2>
+      <h2>Help</h2>
+
+    </div>
+  )
+  
+}
+
+export default Nav
